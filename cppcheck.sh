88c617b5b9011cf10615b653ffb4066e44f4bdd1
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2020-12-30 17:40:24 +0500

    upd cppcheck script

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cppcheck include/ipinfo/*.hpp --std=c++20 \
-                              --enable=all \
-                              --platform=unix64
+cppcheck="/usr/bin/cppcheck "
 
-cppcheck src/ipinfo/*.cpp --std=c++20 \
-                          --enable=all \
-                          --platform=unix64
+check_flags="--std=c++20 \
+             --enable=all"
+
+check_platforms="unix32 \
+                 unix64"
+
+check_files="src/ipinfo/*cpp include/ipinfo/*hpp"
+
+for platform in $check_platforms
+do
+    echo -e "\e[1;36mChecking for \"$platform\"...\e[0m"
+    $cppcheck $check_files $check_flags $platform
+done
