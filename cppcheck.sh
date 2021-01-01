2c7f9a7e7fa831461d0d41f4e1c1aaaf14ffb413
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-01-01 23:50:29 +0500

    upd cppcheck script

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo="/usr/bin/echo"
 cppcheck="/usr/bin/cppcheck "
 
 check_flags="--std=c++20 \
@@ -12,6 +13,6 @@ check_files="src/ipinfo/*cpp include/ipinfo/*hpp"
 
 for platform in $check_platforms
 do
-    echo -e "\e[1;36mChecking for \"$platform\"...\e[0m"
+    $echo -e "\e[1;36mChecking for \"$platform\"...\e[0m"
     $cppcheck $check_files $check_flags $platform
 done
