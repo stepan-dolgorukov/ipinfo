14f7975a28f3feb30f798139d0f2a8c8f50c0d6a
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-01-04 16:38:20 +0500

    upd cppcheck.sh

--- cppcheck.sh
+++ cppcheck.sh
@@ -11,8 +11,13 @@ check_platforms="unix32 \
 
 check_files="src/ipinfo/*cpp include/ipinfo/*hpp"
 
+declare -a colors=(
+    "\e[1;32m" # green
+    "\e[0m"    # reset
+)
+
 for platform in $check_platforms
 do
-    $echo -e "\e[1;36mChecking for \"$platform\"...\e[0m"
+    $echo -e "\e[${colors[0]}Checking for \"$platform\"${colors[1]}:"
     $cppcheck $check_files $check_flags $platform
 done
