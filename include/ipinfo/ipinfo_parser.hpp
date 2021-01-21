ec4868d43fa9ea557935928cb26b153c3e27a83b
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-01-21 12:28:19 +0500

    code polish and correction

--- include/ipinfo/ipinfo_parser.hpp
+++ include/ipinfo/ipinfo_parser.hpp
@@ -15,39 +15,33 @@ namespace ipinfo::service
             ::cJSON* __prepare(const std::string &json);
 
             template<template<typename ...> class T, typename sub_T> void
-            __catch_node(
-                    const ::cJSON &data,
-                    T<sub_T> &node,
-                    const std::string &host);
+            __catch_node(const ::cJSON &data,
+                         T<sub_T> &node,
+                         const std::string &host);
 
             template<template<typename ...> class T> void
-            __fill_node(
-                    T<std::string> &node,
-                    const ::cJSON &item,
-                    const std::string &host);
+            __fill_node(T<std::string> &node,
+                        const ::cJSON &item,
+                        const std::string &host);
 
             template<template<typename ...> class T> void
-            __fill_node(
-                    T<std::int32_t> &node,
-                    const ::cJSON &item,
-                    const std::string &host);
+            __fill_node(T<std::int32_t> &node,
+                        const ::cJSON &item,
+                        const std::string &host);
 
             template<template<typename ...> class T> void
-            __fill_node(
-                    T<double> &node,
-                    const ::cJSON &item,
-                    const std::string &host);
+            __fill_node(T<double> &node,
+                        const ::cJSON &item,
+                        const std::string &host);
 
             template<template<typename ...> class T> void
-            __fill_node(
-                    T<bool> &node,
-                    const ::cJSON &item,
-                    const std::string &host);
+            __fill_node(T<bool> &node,
+                        const ::cJSON &item,
+                        const std::string &host);
         public:
-            void parse(
-                    const std::string &json,
-                    ipinfo::service::types::info &info,
-                    const std::string &host);
+            void parse(const std::string &json,
+                       ipinfo::service::types::info &info,
+                       const std::string &host);
 
             ipinfo::types::error get_last_error(void) const;
     };
