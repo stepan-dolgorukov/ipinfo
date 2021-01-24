8be259bf7a51965e3ade23fc317e844f5ff06548
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-01-24 17:58:46 +0500

    code formatting in example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -27,46 +27,46 @@ app::show_ip_info(
         const std::string &ip,
         const std::string &lang)
 {
-    ipinfo::interface::informer informer{};
-    ipinfo::types::error        error{};
+    ipinfo::user::interface::informer informer{};
+    ipinfo::user::types::error        error{};
 
     informer.set_ip(ip);
     informer.set_lang(lang);
     informer.run();
 
-    fmt::print("IP: {:s}\n", informer.get_ip());
-    fmt::print("IP type: {:s}\n", informer.get_ip_type());
-    fmt::print("Continent: {:s}\n", informer.get_continent());
-    fmt::print("Continent code: {}\n", informer.get_continent_code());
-    fmt::print("County: {:s}\n", informer.get_country());
-    fmt::print("Country code: {:s}\n", informer.get_country_code());
-    fmt::print("Country capital: {:s}\n", informer.get_country_capital());
-    fmt::print("Country phone code: {:s}\n", informer.get_country_ph_code());
-    fmt::print("Country neighbors: {:s}\n", informer.get_country_neighbors());
-    fmt::print("Region: {:s}\n", informer.get_region());
-    fmt::print("Region code: {:s}\n", informer.get_region_code());
-    fmt::print("City: {:s}\n", informer.get_city());
-    fmt::print("City district: {:s}\n", informer.get_city_district());
-    fmt::print("ZIP code: {:s}\n", informer.get_zip_code());
-    fmt::print("Latitude: {:.2f}\n", informer.get_latitude());
-    fmt::print("Longitude: {:.2f}\n", informer.get_longitude());
-    fmt::print("Timezone: {:s}\n", informer.get_timezone());
-    fmt::print("City timezone: {:s}\n", informer.get_city_timezone());
-    fmt::print("Timezone GMT: {:s}\n", informer.get_timezone_gmt());
-    fmt::print("GMT offset: {:d}\n", informer.get_gmt_offset());
-    fmt::print("DST offset: {:d}\n", informer.get_dst_offset());
-    fmt::print("ISP: {:s}\n", informer.get_isp());
-    fmt::print("AS: {:s}\n", informer.get_as());
-    fmt::print("Organization: {:s}\n", informer.get_org());
-    fmt::print("Reverse DNS lookup: {:s}\n", informer.get_reverse_dns());
-    fmt::print("Hosting, data center: {:d}\n", informer.get_hosting_status());
-    fmt::print("Proxy, VPN, Tor usage: {:d}\n", informer.get_proxy_status());
-    fmt::print("Mobile connection usage: {:d}\n", informer.get_mobile_status());
-    fmt::print("Currency: {:s}\n", informer.get_currency());
-    fmt::print("Currency code: {:s}\n", informer.get_currency_code());
-    fmt::print("Currency symbol: {:s}\n", informer.get_currency_symbol());
+    fmt::print("IP: {:s}\n",                              informer.get_ip());
+    fmt::print("IP type: {:s}\n",                         informer.get_ip_type());
+    fmt::print("Continent: {:s}\n",                       informer.get_continent());
+    fmt::print("Continent code: {}\n",                    informer.get_continent_code());
+    fmt::print("County: {:s}\n",                          informer.get_country());
+    fmt::print("Country code: {:s}\n",                    informer.get_country_code());
+    fmt::print("Country capital: {:s}\n",                 informer.get_country_capital());
+    fmt::print("Country phone code: {:s}\n",              informer.get_country_ph_code());
+    fmt::print("Country neighbors: {:s}\n",               informer.get_country_neighbors());
+    fmt::print("Region: {:s}\n",                          informer.get_region());
+    fmt::print("Region code: {:s}\n",                     informer.get_region_code());
+    fmt::print("City: {:s}\n",                            informer.get_city());
+    fmt::print("City district: {:s}\n",                   informer.get_city_district());
+    fmt::print("ZIP code: {:s}\n",                        informer.get_zip_code());
+    fmt::print("Latitude: {:.2f}\n",                      informer.get_latitude());
+    fmt::print("Longitude: {:.2f}\n",                     informer.get_longitude());
+    fmt::print("Timezone: {:s}\n",                        informer.get_timezone());
+    fmt::print("City timezone: {:s}\n",                   informer.get_city_timezone());
+    fmt::print("Timezone GMT: {:s}\n",                    informer.get_timezone_gmt());
+    fmt::print("GMT offset: {:d}\n",                      informer.get_gmt_offset());
+    fmt::print("DST offset: {:d}\n",                      informer.get_dst_offset());
+    fmt::print("ISP: {:s}\n",                             informer.get_isp());
+    fmt::print("AS: {:s}\n",                              informer.get_as());
+    fmt::print("Organization: {:s}\n",                    informer.get_org());
+    fmt::print("Reverse DNS lookup: {:s}\n",              informer.get_reverse_dns());
+    fmt::print("Hosting, data center: {:d}\n",            informer.get_hosting_status());
+    fmt::print("Proxy, VPN, Tor usage: {:d}\n",           informer.get_proxy_status());
+    fmt::print("Mobile connection usage: {:d}\n",         informer.get_mobile_status());
+    fmt::print("Currency: {:s}\n",                        informer.get_currency());
+    fmt::print("Currency code: {:s}\n",                   informer.get_currency_code());
+    fmt::print("Currency symbol: {:s}\n",                 informer.get_currency_symbol());
     fmt::print("Currency exchange rate to USD: {:.2f}\n", informer.get_currency_rates());
-    fmt::print("Currency plural: {:s}\n", informer.get_currency_plural());
+    fmt::print("Currency plural: {:s}\n",                 informer.get_currency_plural());
 
     return;
 }
@@ -76,15 +76,15 @@ app::show_ip_info_ex(
         const std::string &ip,
         const std::string &lang)
 {
-    ipinfo::interface::informer informer{ip, lang, 0u};
-    ipinfo::types::error        error{};
+    ipinfo::user::interface::informer       informer{ip, lang, 0u};
+    ipinfo::user::types::error              error{};
+
+    ipinfo::user::types::node<std::string>  str_cont{};
+    ipinfo::user::types::node<std::int32_t> i32_cont{};
+    ipinfo::user::types::node<double>       dbl_cont{};
+    ipinfo::user::types::node<bool>         bool_cont{};
 
     informer.run();
-
-    ipinfo::types::node<std::string>  str_cont{};
-    ipinfo::types::node<std::int32_t> i32_cont{};
-    ipinfo::types::node<double>       dbl_cont{};
-    ipinfo::types::node<bool>         bool_cont{};
 
     // printing below isn't such of good
     // we need to find a compact solution
