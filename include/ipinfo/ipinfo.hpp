9592c37bee746bfab47b8ccbbbb5c580a6d07534
Stepan Dolgorukov <dolgorukovpro@gmail.com>
2021-03-17 17:23:12 +0500

    change include order

--- include/ipinfo/ipinfo.hpp
+++ include/ipinfo/ipinfo.hpp
@@ -1,9 +1,9 @@
 #ifndef IPINFO_HPP
     #define IPINFO_HPP
 
-#include "ipinfo_types.hpp"
 #include "ipinfo_constants.hpp"
-#include "ipinfo_informer.hpp"
+#include "ipinfo_types.hpp"
 #include "ipinfo_aliases.hpp"
+#include "ipinfo_informer.hpp"
 
 #endif // IPINFO_HPP
